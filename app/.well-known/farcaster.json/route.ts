7fa9e9b5bb8b52783338349e3521b59cf2628c8a
NinjasPyajamas <divyansh242805@gmail.com>
2025-11-16 01:02:22 +0530

    added all

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,20 +4,32 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association
+    // {
+
+    "accountAssociation": {
+      "header": "eyJmaWQiOjEzMTcwNzEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHg5REZDMjIyZDc4NDg1QjgzRTQ4OWEwNENBZDU2NDE3QWFBOTI0NzY4In0",
+      "payload": "eyJkb21haW4iOiJmcmFwZWQtbmZ0LnZlcmNlbC5hcHAifQ",
+      "signature": "HZIXbig06Wi78aWVtnAfiItSr4o5/Req/+8ntViPEZ4iSB/qpkq91UBwCQflmlqIZ78s7O4ao+LyhAip9lmK3Bs="
+        },
+    // },
     frame: {
       version: "1",
-      name: "Monad Farcaster MiniApp Template",
+      name: "Ape Run",
       iconUrl: `${APP_URL}/images/icon.png`,
       homeUrl: `${APP_URL}`,
       imageUrl: `${APP_URL}/images/feed.png`,
       screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
+      tags: ["nft", "farcaster", "mint", "FarApe"],
+      primaryCategory: "social",
+      buttonTitle: "Launch Ape Run",
       splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
+      splashBackgroundColor: "#0f172a",
       webhookUrl: `${APP_URL}/api/webhook`,
+      requiredChains: ["eip155:42161"],
     },
+    "baseBuilder": {
+      "allowedAddresses": ["0xE7503b8d192DcE2895327878ECE5a0a401821a66"]
+    }
   };
 
   return NextResponse.json(farcasterConfig);
